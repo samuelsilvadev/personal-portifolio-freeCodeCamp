823e7cc68d5c54328f5c781c36096e1174da271b
samuel <samuelsilvawb@gmail.com>
2017-06-06 05:11:43 -0300

    Usando plugin para renomear arquivos

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const rename = require('gulp-rename');
 
 const scssFiles = './scss/style.scss';
 const cssDest = './css';
+const cssMinDest = './dist/css';
 
 const sassDevOptions = {
   outputStyle: 'expanded'
@@ -14,14 +16,15 @@ const sassProdOptions = {
 
 gulp.task('sassDev', () => {
     return gulp.src(scssFiles)
-        .pipe(sass(sassDevOptions))
+        .pipe(sass(sassDevOptions).on('error', sass.logError))
         .pipe(gulp.dest(cssDest));
 });
 
 gulp.task('sassProd', () => {
     return gulp.src(scssFiles)
-        .pipe(sass(sassProdOptions))
-        .pipe(gulp.dest(cssDest));
+        .pipe(sass(sassProdOptions).on('error', sass.logError))
+        .pipe(rename('style.min.css'))
+        .pipe(gulp.dest(cssMinDest));
 });
 
 gulp.task('default', () => {
