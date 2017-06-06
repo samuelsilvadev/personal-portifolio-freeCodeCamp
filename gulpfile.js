87619d02393393e87fed5af513e2e3a84bcd8799
samuel <samuelsilvawb@gmail.com>
2017-06-06 05:08:11 -0300

    Registranco tarefa do SASS

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,30 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 
+const scssFiles = './scss/style.scss';
+const cssDest = './css';
+
+const sassDevOptions = {
+  outputStyle: 'expanded'
+}
+
+const sassProdOptions = {
+  outputStyle: 'compressed'
+}
+
+gulp.task('sassDev', () => {
+    return gulp.src(scssFiles)
+        .pipe(sass(sassDevOptions))
+        .pipe(gulp.dest(cssDest));
+});
+
+gulp.task('sassProd', () => {
+    return gulp.src(scssFiles)
+        .pipe(sass(sassProdOptions))
+        .pipe(gulp.dest(cssDest));
+});
+
 gulp.task('default', () => {
   console.log('Gulp funcionando...');
 });
+
